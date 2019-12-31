0123f51cf950adf82c327d9f608447db7af2508f
hjltest <343323692@qq.com>
2019-12-31 20:28:26 +0800

    first commit

--- app/src/main/java/com/example/thundersoft/camera/Camera2Activity.java
+++ app/src/main/java/com/example/thundersoft/camera/Camera2Activity.java
@@ -6,12 +6,16 @@ import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
 import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -19,6 +23,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
@@ -27,7 +32,13 @@ import android.widget.Toast;
 
 import com.example.thundersoft.camera.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class Camera2Activity extends AppCompatActivity {
@@ -43,6 +54,12 @@ public class Camera2Activity extends AppCompatActivity {
     private CameraCaptureSession mSession;
 
     private ImageReader mImageReader;
+//    private File mFile;
+//    private Image mImage;
+
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,14 +69,10 @@ public class Camera2Activity extends AppCompatActivity {
         mClick = findViewById(R.id.click);
         mTextureView.setSurfaceTextureListener(surfaceTextureListener);
 
-        mImageReader = ImageReader.newInstance(1280, 720, ImageFormat.JPEG, 2);
-        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
-            @Override
-            public void onImageAvailable(ImageReader reader) {
-                Log.d("camera_api2", "take picture!");
-                Image image = reader.acquireLatestImage();
-            }
-        }, null);
+//        mImageReader = ImageReader.newInstance(1280, 720, ImageFormat.JPEG, 2);
+
+        startBackgroundThread();
+
         mClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,6 +111,19 @@ public class Camera2Activity extends AppCompatActivity {
         try {
             mCameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
             mCameraManager.openCamera("0", mStateCallback, null);
+
+            //设置ImageReader,将大小，图片格式
+            StreamConfigurationMap map = null;
+            try {
+                map = mCameraManager.getCameraCharacteristics("0").get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+            Size largest = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)), new CompareSizesByArea());
+
+            mImageReader = ImageReader.newInstance(largest.getWidth(), largest.getHeight(), ImageFormat.JPEG, /*maxImages*/2);
+            mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, null);
+
 
         } catch (CameraAccessException e) {
             e.printStackTrace();
@@ -143,6 +169,8 @@ public class Camera2Activity extends AppCompatActivity {
             try {
                 mSession = session;
                 mSession.setRepeatingRequest(mRequest, null, null);
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -204,4 +232,76 @@ public class Camera2Activity extends AppCompatActivity {
             default:
         }
     }
+
+    class CompareSizesByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            Log.d(MainActivity.TAG, "compare: ");
+            // We cast here to ensure the multiplications won't overflow
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                    (long) rhs.getWidth() * rhs.getHeight());
+        }
+
+    }
+
+    private static class ImageSaver implements Runnable {
+
+        /**
+         * The JPEG image
+         */
+        private final Image mImage;
+        /**
+         * The file we save the image into.
+         */
+        private final File mFile;
+
+        ImageSaver(Image image, File file) {
+            mImage = image;
+            mFile = file;
+        }
+
+        @Override
+        public void run() {
+            Log.d(MainActivity.TAG, "run: ");
+            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            FileOutputStream output = null;
+            try {
+                output = new FileOutputStream(mFile);
+                output.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mImage.close();
+                if (null != output) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void startBackgroundThread() {
+        Log.d(MainActivity.TAG, "startBackgroundThread: ");
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private final ImageReader.OnImageAvailableListener mOnImageAvailableListener
+            = new ImageReader.OnImageAvailableListener() {
+
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Log.d(MainActivity.TAG, "onImageAvailable: ");
+            mBackgroundHandler.post(new ImageSaver(reader.acquireNextImage(), new File(getExternalFilesDir(null), "pic.jpg")));
+        }
+
+    };
 }
